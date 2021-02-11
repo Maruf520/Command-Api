6e18ccdf6f99c3cb63473a50a39fc09b700ac146
Md Maruf <maaruf.bs23@gmail.com>
2021-02-12 00:22:51 +0600

    DbContext Added

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -1,7 +1,9 @@
+using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,10 @@ namespace Commander
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddDbContext<CommanderContext>(options =>
+
+          options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
