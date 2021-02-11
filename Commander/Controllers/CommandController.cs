f82984ff0016b7a8a5fc6adbdba59656f7b2e511
Md Maruf <maaruf.bs23@gmail.com>
2021-02-11 23:38:15 +0600

    Add project files.

---
+++ Commander/Controllers/CommandController.cs
@@ -0,0 +1,33 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItem = _repository.GetAppCommad();
+            return Ok(commandItem);
+        }
+
+        //Get api/command/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,35 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommad()
+        {
+            var commad = new List<Command>
+            {
+                new Command {Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle & pan"},
+                new Command {Id = 1, HowTo = "CUt bread", Line = "Get a Knife", Platform = "Knife and chopping board"},
+                new Command {Id = 2, HowTo = "Make Cup of tea", Line = "palce tea bag in cup", Platform = "Kettle & Cup"},
+                new Command {Id = 3, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle & pan"},
+            };
+            return commad;
+        }
+
+ 
+        public Command GetCommandById(int id)
+        {
+            /*            var command = new Command
+                        {
+                            Id = 0, HowTo = "sdgf", Line = "Boil water", Platform = "Kettle & pan"
+                        };
+                        return command;*/
+
+           return  new Command { Id = 0, Line = "sdfs", HowTo = "sgsg", Platform = "dfgfdged" };
+        }
+    }
+}
