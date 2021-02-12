c00f1e9a606ac314fe6007035a6651e0e85711f0
Md Maruf <maaruf.bs23@gmail.com>
2021-02-12 15:58:35 +0600

    Dtos Created

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -32,6 +32,7 @@ namespace Commander
             services.AddDbContext<CommanderContext>(options =>
 
           options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
