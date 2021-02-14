1792e3faa5565cbfd7f06c2286169099294527bf
Md Maruf <maaruf.bs23@gmail.com>
2021-02-14 12:04:46 +0600

    Route Return Done for single data in CommandController

--- Commander/Controllers/CommandController.cs
+++ Commander/Controllers/CommandController.cs
@@ -10,7 +10,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Commander.Controllers
-{
+{ 
+
     [Route("api/commands")]
     [ApiController]
     public class CommandController : ControllerBase
@@ -32,7 +33,7 @@ namespace Commander.Controllers
         }
 
         //Get api/command/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}",Name="GetCommandById")]
         public ActionResult <CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
@@ -50,7 +51,10 @@ namespace Commander.Controllers
             var commandModel = _mapper.Map<Command>(commandCreateDto);
             _repository.CreateCommand(commandModel);
             _repository.SaveChanges();
-            return Ok(commandModel);
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
+         /*   return Ok(commandReadDto);*/
         }
     }
 }
