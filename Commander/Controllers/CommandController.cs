b8ce3f5e7e177315da8e3bbf28fe048e03ead068
Md Maruf <maaruf.bs23@gmail.com>
2021-02-14 15:52:55 +0600

    HttpPut Endpoint Created

--- Commander/Controllers/CommandController.cs
+++ Commander/Controllers/CommandController.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Commander.Controllers
-{ 
+{
 
     [Route("api/commands")]
     [ApiController]
@@ -22,22 +22,22 @@ namespace Commander.Controllers
         {
             _repository = repository;
             _mapper = imapper;
-                
+
         }
 
         [HttpGet]
-        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItem = _repository.GetAllCommad();
             return Ok(commandItem);
         }
 
         //Get api/command/{id}
-        [HttpGet("{id}",Name="GetCommandById")]
-        public ActionResult <CommandReadDto> GetCommandById(int id)
+        [HttpGet("{id}", Name = "GetCommandById")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
-            if(commandItem != null)
+            if (commandItem != null)
             {
                 return Ok(_mapper.Map<CommandReadDto>(commandItem));
             }
@@ -49,12 +49,35 @@ namespace Commander.Controllers
         public ActionResult<CommandCreateDto> CreateCommand(CommandCreateDto commandCreateDto)
         {
             var commandModel = _mapper.Map<Command>(commandCreateDto);
+
             _repository.CreateCommand(commandModel);
+
             _repository.SaveChanges();
 
             var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
             return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
-         /*   return Ok(commandReadDto);*/
+            /*   return Ok(commandReadDto);*/
+        }
+
+
+        //PUT api/commands
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            var commandModelFormRepo = _repository.GetCommandById(id);
+
+            if (commandUpdateDto == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(commandUpdateDto, commandModelFormRepo);
+
+            _repository.UpdateCommand(commandModelFormRepo);
+
+            _repository.SaveChanges();
+
+            return NoContent();
         }
     }
 }
---
+++ Commander/Migrations/20210214062225_annotation.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Commander.Migrations
+{
+    public partial class annotation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Platform",
+                table: "commands",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Line",
+                table: "commands",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HowTo",
+                table: "commands",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Platform",
+                table: "commands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Line",
+                table: "commands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HowTo",
+                table: "commands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+    }
+}
